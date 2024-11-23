d43bff8c9d2b3ff58948260c242e079c06028a4f
Im-Siyoun <rlatldbs3321@sju.ac.kr>
2024-11-23 21:38:52 +0900

    feat : 일기 조회 라우팅팅

--- src/routes/diary.route.js
+++ src/routes/diary.route.js
@@ -4,5 +4,6 @@ import * as diaryControllers from "../controllers/diary.controller.js";
 const diaryRouter = express.Router();
 
 diaryRouter.post('/', diaryControllers.writeDiary);
+diaryRouter.get('/:userId', diaryControllers.getDiary);
 
 export default diaryRouter;
